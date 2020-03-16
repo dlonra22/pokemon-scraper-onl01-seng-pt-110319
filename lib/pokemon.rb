7fcf89b0f303e8150b588e5576e559b1f613ed4f
arnie b <arnold.bwaila@gmail.com>
2020-03-16 11:16:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,9 +12,6 @@ class Pokemon
     
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)",name, type)
     @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-    poke = self
-    end
-    poke
   end
   
   def self.find(id, db)
